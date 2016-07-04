5a441b8668c0a8dcfb6a34acf3302e8996d6c5ad
danqing2k <danqing2k@hotmail.com>
2016-07-04 17:48:38 +0200

    Fixed textview size

--- DW2App/DW2App/PartViewController.swift
+++ DW2App/DW2App/PartViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class PartViewController: UIViewController {
 
+    @IBOutlet weak var Text: UITextView!
     @IBOutlet weak var ArtistButton: UIButton!
     @IBOutlet weak var ArtworkButton: UIButton!
     
@@ -19,6 +20,7 @@ class PartViewController: UIViewController {
         super.viewDidLoad()
         ArtistButton.layer.cornerRadius = 5
         ArtworkButton.layer.cornerRadius = 5
+        Text.editable = false
 
         // Do any additional setup after loading the view.
     }
