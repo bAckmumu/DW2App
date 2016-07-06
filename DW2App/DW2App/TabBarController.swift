d3fab8fa9231747afdcc081ccd0c41b44ff83bcf
danqing2k <danqing2k@hotmail.com>
2016-07-06 18:59:27 +0200

    TabBar color Changed

---
+++ DW2App/DW2App/TabBarController.swift
@@ -0,0 +1,25 @@
+//
+//  TabBarController.swift
+//  DW2App
+//
+//  Created by danqing liu on 16/7/6.
+//  Copyright © 2016年 DW2. All rights reserved.
+//
+import UIKit
+
+class TabBarController: UITabBarController {
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        //  self.tabBar.tintColor = UIColor(red: 187/255, green: 14/255, blue: 25/255, alpha: 1)
+        self.tabBar.tintColor = UIColor(red: 255/255, green: 255/255, blue: 255/255, alpha: 1)
+        self.tabBar.barTintColor = UIColor(red: 30/255, green: 30/255, blue: 30/255,alpha: 1)
+        
+        
+        
+        
+        
+    }
+    
+}
