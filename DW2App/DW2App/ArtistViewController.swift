86092a1f2d0a670060ec8f364e6e87dfa2113ad4
danqing2k <danqing2k@hotmail.com>
2016-07-07 08:51:29 +0200

    ArtistView layout fixed

--- DW2App/DW2App/ArtistViewController.swift
+++ DW2App/DW2App/ArtistViewController.swift
@@ -13,6 +13,7 @@ class ArtistViewController: UIViewController {
     @IBOutlet var nameImage: UIImageView!
     @IBOutlet var artistImage: UIImageView!
     
+    @IBOutlet weak var toArtwork_Button: UIButton!
     var artist: String = ""
     
     override func viewDidLoad() {
@@ -22,6 +23,11 @@ class ArtistViewController: UIViewController {
                 
         nameImage.image = UIImage(named: artist + "_name")
         artistImage.image = UIImage(named: artist + "_artist")
+        self.artistImage.contentMode = UIViewContentMode.ScaleAspectFit
+        toArtwork_Button.setTitle("Kunstwerke", forState: UIControlState.Normal)
+        toArtwork_Button.layer.cornerRadius = 5
+        toArtwork_Button.backgroundColor = UIColor(red: 187/255, green: 14/255, blue: 25/255, alpha: 1)
+        toArtwork_Button.tintColor = UIColor.whiteColor()
         
     }
 
